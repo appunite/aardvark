ffc3a98cffd65426d95dbfa8a742818ea42bf1dc
Marcin Operacz <marcin.operacz@gmail.com>
2025-10-26 05:23:38 +0800

    Use doc_cfg for docs.rs builds

--- crates/aardvark-core/src/lib.rs
+++ crates/aardvark-core/src/lib.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 //! Aardvark is an embeddable multi-language runtime for executing sandboxed bundles inside V8.
 //!
